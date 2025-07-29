a0de4b24224c6c29e3597ac7573a204890676b9b
Mkrtchyan1991 <sev.jan1991@mail.de>
2025-07-29 18:07:55 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
 
-    return redirect('/home');
+    return redirect('/');
 })->middleware(['signed'])->name('verification.verify');
 
 
