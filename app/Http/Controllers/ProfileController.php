a8d6f7fd146616bec44ee319eb8bf45eecd98997
Narek <NSarg1@hotmail.com>
2025-07-31 08:39:07 +0400

    Fix ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,41 +9,41 @@ use Illuminate\Support\Facades\Hash;
 class ProfileController extends Controller
 {
     public function profile()
-{
-    return new UserResource(auth()->user());
-}
-
-public function updateProfile(Request $request)
-{
-    $user = auth()->user();
-
-    $request->validate([
-        'name' => 'required|string|max:255',
-        'last_name' => 'required|string|max:255',
-        'country' => 'required|string|max:100',
-        'address' => 'required|string|max:100',
-        'city' => 'required|string|max:100',
-        'postal_code' => 'required|number|max:10',
-        'mobile_number' => 'string|max:15',
-        'email' => "required|email|unique:users,email,{$user->id}",
-        'password' => 'nullable|min:6|confirmed',
-    ]);
-
-    $user->name = $request->name;
-    $user->email = $request->email;
-    $user->last_name = $request->last_name;
-    $user->country = $request->country;
-    $user->address = $request->address;
-    $user->city = $request->city;
-    $user->postal_code = $request->postal_code;
-    $user->mobile_number = $request->mobile_number;
-
-    if ($request->filled('password')) {
-        $user->password = Hash::make($request->password);
+    {
+        return new UserResource(auth()->user());
     }
 
-    $user->save();
+    public function updateProfile(Request $request)
+    {
+        $user = auth()->user();
 
-    return new UserResource($user);
-}
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'country' => 'required|string|max:100',
+            'address' => 'required|string|max:100',
+            'city' => 'required|string|max:100',
+            'postal_code' => 'required|string|max:10', // Changed from 'number' to 'string'
+            'mobile_number' => 'string|max:15',
+            'email' => "required|email|unique:users,email,{$user->id}",
+            'password' => 'nullable|min:6|confirmed',
+        ]);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->last_name = $request->last_name;
+        $user->country = $request->country;
+        $user->address = $request->address;
+        $user->city = $request->city;
+        $user->postal_code = $request->postal_code;
+        $user->mobile_number = $request->mobile_number;
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->save();
+
+        return new UserResource($user);
+    }
 }
