7b0465f934c38502a98de10b3dffbfce9d03e7a7
Narek <NSarg1@hotmail.com>
2025-08-01 17:25:08 +0400

    Url fix

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
         'http://localhost:3000',  // Common React port
         'http://127.0.0.1:5173',
         'http://127.0.0.1:3000',
-        'https://ms-lily-admin.koyeb.app/', // Your production domain
+        'https://ms-lily-admin.koyeb.app', // Your production domain
     ],
 
     'allowed_origins_patterns' => [],
